157635cfceab95640de45c43b839c52ef3e16793
InsertMusicHere <smtrlive27@gmail.com>
2020-08-27 12:56:53 +0530

    message

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -19,11 +19,9 @@ from django.conf.urls.static import static
 from django.conf import settings
 from portfolio import views
 
-urlpatterns = [
-    #changed admin/ to rex/
-    path('batman/', admin.site.urls),
+urlpatterns =[path('admin/', admin.site.urls),
     path('', views.home,name='home'),
-    path('blog/', include('blog.urls'))
-]
+    path('blog/', include('blog.urls')),
+    ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
